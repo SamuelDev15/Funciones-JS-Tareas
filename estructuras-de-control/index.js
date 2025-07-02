75c7dc4b85e670eb2659b841e94e678246f5a1ec
SamuelDev15 <cruzcruzsamuel64@gmail.com>
2025-07-01 21:22:53 -0500

    Agregados ejercicios if else y for

---
+++ estructuras-de-control/index.js
@@ -0,0 +1,39 @@
+//! EJEMPLOS DE USO DE IF ELSE
+
+//? IF y ELSE sobre si es fin de semana o no:
+
+let finSemana = true;
+
+console.log("Este es el if else sobre si es fin de semana o no:");
+
+if (finSemana) {
+    console.log("Estamos en fin se semana")
+} else {
+    console.log("Estamos en los dias de la semana")
+};
+
+//? IF y ELSE sonbre si hay cupo para mas estudiantes en el aula o no:
+
+let estudiantesDelAula = 25;
+
+console.log("Este es el if else sobre si hay cupo o no en el aula de un colegio:");
+
+if (estudiantesDelAula >= 30) {
+    console.log("En este aula no hay espacio para mas estudiantes")
+} else {
+    console.log("En este aula hay cupo para cinco estudiantes mas")
+};
+
+//! EJEMPLOS DE USO DE FOR ACCEDIENDO A UN ARRAY POR POSICION
+//* Con nombres de Teletubiies jeje :D
+
+let nombresTeletubbies = [
+    "Tinky Winky",
+    "Dipsy",
+    "Laa-Laa",
+    "Po",
+];
+
+for (i = 0; i <= 3; i++) {
+    console.log(nombresTeletubbies[i])
+};
